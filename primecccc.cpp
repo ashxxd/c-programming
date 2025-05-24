ca2a27b5eb6df81f02cafa0b47b42007011ced25
Anonymous <target.success10@gmail.com>
2025-05-24 10:43:58 +0530

    abstract and inheritence

---
+++ primecccc.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    int count=0;
+    int num=2;
+
+    while(count<=69)
+    {
+        factor=0;
+    } 
+    for(int i=1;)
+    {
+        cout<<"prime";
+    }
+    else
+    {
+        cout<<"not prime";
+    }
+
+    return 0;
+}
